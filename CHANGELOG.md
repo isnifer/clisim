35a4558142029a71bae48e0c7a2c181e209c14dd
Anton Kuznetsov <isnifer@gmail.com>
2019-11-23 22:02:48 +0300

    [2.5.0] Updated method to find `ANDROID_SDK_ROOT` instead of `ANDROID_HOME`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,15 @@
+## 2.5.0
+
+### Updated
+
+- Method to find `ANDROID_SDK_ROOT` instead of `ANDROID_HOME` ([#1](https://github.com/isnifer/clisim/pull/1)) ([@n0izn0iz](https://github.com/n0izn0iz))
+
+## 2.4.0
+
+### Updated
+
+- Deps
+
 ## 2.3.1
 
 ### Removed
--- core/findAndroidEmulators.js
+++ core/findAndroidEmulators.js
@@ -1,26 +1,32 @@
 const { execSync } = require('child_process')
 const fs = require('fs')
 
-function isValidAndroidSdk(sdkRoot) {
-  if (!sdkRoot) return false
-  // not sure if this is a good test for a valid sdk
-  // but since we only use emulator/emulator, it should be good enough
-  const emulator = `${sdkRoot}/emulator/emulator`
-  return fs.existsSync(emulator)
+function getValidAndroidSdk(memo, sdkRoot) {
+  if (!memo) {
+    const path = `${sdkRoot}/emulator/emulator`
+    if (fs.existsSync(path)) {
+      return path
+    }
+  }
+
+  return memo
 }
 
-function getAndroidSdkRoot() {
-  // see https://developer.android.com/studio/command-line/variables
-  const { ANDROID_HOME } = process.env
-  if (isValidAndroidSdk(ANDROID_HOME)) return ANDROID_HOME
+function getEmulatorPath() {
+  // @see https://developer.android.com/studio/command-line/variables
+  const { ANDROID_HOME, ANDROID_SDK_ROOT } = process.env
+  const emulatorPath = [ANDROID_HOME, ANDROID_SDK_ROOT].reduce(getValidAndroidSdk, false)
 
-  const { ANDROID_SDK_ROOT } = process.env
-  if (isValidAndroidSdk(ANDROID_SDK_ROOT)) return ANDROID_SDK_ROOT
+  if (emulatorPath) {
+    return emulatorPath
+  }
 
   console.error(
-    `Can not find a valid android sdk in ANDROID_HOME (${ANDROID_HOME}) or ANDROID_SDK_ROOT (${ANDROID_SDK_ROOT})`
+    `Can not find a valid Android SDK in ANDROID_HOME (${ANDROID_HOME})' +
+    ' or ANDROID_SDK_ROOT (${ANDROID_SDK_ROOT})`
   )
-  throw new Error('Android sdk not found')
+
+  throw new Error('Android SDK not found')
 }
 
 module.exports = async function findAndroidEmulators(asyncCallback) {
@@ -34,11 +40,12 @@ module.exports = async function findAndroidEmulators(asyncCallback) {
       throw new Error('Devices list is empty')
     }
 
-    const { name } = await asyncCallback(devices)
+    const { name: deviceName } = await asyncCallback(devices)
+    const emulatorPath = getEmulatorPath()
 
-    return execSync(`${getAndroidSdkRoot()}/emulator/emulator @${name}`)
+    return execSync(`${emulatorPath} @${deviceName}`)
   } catch (error) {
-    console.error('Can not find any android devices') // eslint-disable-line
+    console.error(error || 'Can not find any android devices') // eslint-disable-line
   }
 
   return false
